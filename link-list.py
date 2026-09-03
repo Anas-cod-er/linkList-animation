0c86cc9963c8e474efc24ce80890d36fdeeb621d
Anas-cod-er <anasbinjaman100@gmail.com>
2026-09-03 22:33:44 +0800

    Add Double linklist

--- link-list.py
+++ link-list.py
@@ -21,13 +21,17 @@ class Linklist(Scene):
             Text("40")
         )
         node5 = VGroup(
-            Square(side_length=1.5),
-            Text("Null")
+            Square(side_length=1),
+            Text("Null", font_size=20)
+        )
+        node6 = VGroup(
+            Square(side_length=1),
+            Text("Null", font_size=20)
         )
 
 
         #arrage nodes horizontally
-        nodes = VGroup(node1,node2,node3,node4,node5)
+        nodes = VGroup(node6,node1,node2,node3,node4,node5)
         nodes.arrange(RIGHT, buff=1) ##buff is gap
 
 
@@ -54,9 +58,42 @@ class Linklist(Scene):
             node5.get_left(),
             buff=0.1
         )
+        arrownull = Arrow(
+            node1.get_left(),
+            node6.get_right(),
+            buff= 0.1
+        )
+
+
+        arrowrev3 = Arrow(
+            node4.get_left(),
+            node3.get_right(),
+            buff=0.1
+        )
+        arrowrev2 =Arrow(
+            node3.get_left(),
+            node2.get_right(),
+            buff=0.1
+        )
+        arrowrev1 = Arrow(
+            node2.get_left(),
+            node1.get_right(),
+            buff=0.1
+        )
+        
+
+        arrow3.shift(UP * 0.1)
+        arrowrev3.shift(DOWN *0.1)
+
+        arrow2.shift(UP * 0.1)
+        arrowrev2.shift(DOWN * 0.1)
+
+        arrow1.shift(UP *0.1)
+        arrowrev1.shift(DOWN *0.1)
+
 
         #create text
-        text = Text("Single Link List")
+        text = Text("Double Link List")
         text.to_edge(DOWN)
 
         #animate text
@@ -77,6 +114,14 @@ class Linklist(Scene):
         self.play(Create(arrow4))
 
         self.play(Create(node5))
+
+        self.play(Create(arrowrev3))
+        self.play(Create(arrowrev2))
+        self.play(Create(arrowrev1))
+
+        self.play(Create(node6))
+        self.play(Create(arrownull))
+
 
         
 
